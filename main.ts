dfac6d9046607687d5966f1cf94574d0244b971e
sethpcodeninjas <79381106+sethpcodeninjas@users.noreply.github.com>
2021-04-19 16:36:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,239 @@
+namespace SpriteKind {
+    export const Grocery = SpriteKind.create()
+    export const CartItem = SpriteKind.create()
+}
+function createAllGroceries(){
+    for (){
+        
+    }
+}
+function createGrocery (gImg: Image, cost: number, weight: number, name: string) {
+    g = sprites.create(gImg, SpriteKind.Grocery)
+    // Set data for sprite
+    sprites.setDataString(g, "name", name)
+    sprites.setDataNumber(g, "cost", cost)
+    sprites.setDataNumber(g, "weight", weight)
+    tiles.placeOnRandomTile(g, assets.tile`tile1`)
+}
+let g: Sprite = null
+let groceryImages = [
+img`
+    . . . 2 2 2 . . . . . . . . . . 
+    . . . c c c 6 6 8 8 . . . . . . 
+    . . 6 1 1 1 1 1 9 6 8 . . . . . 
+    . 6 1 1 1 1 1 1 8 9 6 8 . . . . 
+    6 1 1 1 1 1 1 8 . 8 9 8 . . . . 
+    6 1 1 1 1 1 1 8 . 8 9 8 . . . . 
+    8 9 1 1 1 1 1 8 . 8 9 8 . . . . 
+    8 9 1 1 1 1 1 8 8 9 9 8 . . . . 
+    8 9 9 9 9 9 9 9 9 9 9 8 . . . . 
+    8 6 9 9 9 9 9 9 9 9 9 8 . . . . 
+    . 8 6 9 9 9 9 9 9 9 6 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . 6 6 6 . . . . . . 
+    . . . . . . . c b c . . . . . . 
+    . . . . . c c c b c c c . . . . 
+    . . . . c b b b b b b b c . . . 
+    . . . . c 1 b b b b b 1 c . . . 
+    . . . . c 1 1 7 1 7 1 1 c . . . 
+    . . . . c 1 1 1 7 1 1 1 c . . . 
+    . . . . c 1 1 a c a 1 1 c . . . 
+    . . . . c 1 a c a c a 1 c . . . 
+    . . . . c 1 c a c a c 1 c . . . 
+    . . . . c 1 a c a c a 1 c . . . 
+    . . . . c 1 c a c a 1 1 c . . . 
+    . . . . c 1 a c a 1 1 1 c . . . 
+    . . . . c b 1 a 1 1 1 b c . . . 
+    . . . . c b b b b b b b c . . . 
+    . . . . . c c c c c c c . . . . 
+    `,
+img`
+    . c c c c c c c c c c c c c . . 
+    c b b b b b b b b b b b b b c . 
+    c b b b b b b b b b b b b b c . 
+    c c c c c c c c c c c c c c c . 
+    c d d 1 1 1 1 1 1 1 1 1 d d c . 
+    c d c c c 1 c c c 1 c c c d c . 
+    c d c 1 c 1 c 1 c 1 1 c d d c . 
+    c d c 1 c 1 c c c 1 1 c d d c . 
+    c d c c c 1 c 1 c 1 1 c d d c . 
+    c d d 1 1 1 1 1 1 1 1 1 d d c . 
+    c d d 1 1 1 2 2 2 1 1 1 d d c . 
+    c d d 1 1 2 8 8 8 2 1 1 d d c . 
+    c d d 1 1 2 8 d 8 2 1 1 d d c . 
+    c d d 1 1 2 8 6 8 2 1 1 d d c . 
+    . c d 1 1 1 2 2 2 1 1 1 d c . . 
+    . . c c c c c c c c c c c . . . 
+    `,
+img`
+    . . . c c c c . . . . . . . . . 
+    . . c e e e e c c c . . . . . . 
+    . c e e e e e e e e c . . . . . 
+    . c e e e e e e e e e c . . . . 
+    f e e e e c c c e e e c . b b . 
+    f e e e c e e e c c c c c d d b 
+    f c e e f e e e e e e e c d d b 
+    f c c c f e e e f f f f c b b b 
+    f c c c c f f f c c c f . c c . 
+    . f c c c c c c c c c f . . . . 
+    . f c c c c c c c c f . . . . . 
+    . . f f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . 6 6 9 9 9 9 . . . . . . . . 
+    . 6 9 9 1 1 1 1 9 . . . . . . . 
+    6 9 6 9 9 9 1 9 1 9 . . . . . . 
+    6 9 9 6 6 6 6 1 1 9 . . . . . . 
+    6 9 9 6 9 9 6 1 1 9 . . . . . . 
+    . 6 9 6 9 9 6 1 9 . . . . . . . 
+    . . 6 9 6 6 1 9 . . . . . . . . 
+    . . . 6 9 9 9 . . . . . . . . . 
+    . . . . 6 6 . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c c c c . . . . . 
+    . . . c d d d d d d d c . . . . 
+    . . c d d d d d b b d d c . . . 
+    . . b c d d d d d d d c b . . . 
+    . . b 2 c c c c c c c 2 b . . . 
+    . . b 2 2 2 2 2 2 2 2 2 b . . . 
+    . . b 2 2 2 2 2 2 2 2 2 b . . . 
+    . . b 2 2 2 b b b 2 2 2 b . . . 
+    . . b 2 2 b 2 2 2 b 2 2 b . . . 
+    . . d 1 2 b 2 2 2 b 2 1 d . . . 
+    . . d 1 1 b 2 2 2 b 1 1 d . . . 
+    . . d 1 1 b 2 2 2 b 1 1 d . . . 
+    . . d 1 1 1 b b b 1 1 1 d . . . 
+    . . . d 1 1 1 1 1 1 1 d . . . . 
+    . . . . d d d d d d d . . . . . 
+    `,
+img`
+    . . c c c c c c c c c c . . . . 
+    . c d d d d d d d c b b c . . . 
+    c d d d d d d d c b d b b c . . 
+    c c d d d d d d d c b b c c . . 
+    c b c c c c c c c c c c b c . . 
+    c b 8 9 8 b 8 9 9 9 8 b b c . . 
+    c b b 8 9 6 9 6 9 6 9 8 b c . . 
+    c b b 8 9 6 9 6 9 6 9 8 b c . . 
+    c b 8 9 8 b 8 9 9 9 8 b b c . . 
+    . c c c c c c c c c c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . b 1 1 1 1 1 1 1 1 1 . . . 
+    . . b 1 1 1 1 1 1 1 1 1 1 1 . . 
+    . . b 1 1 1 1 1 1 1 1 1 8 8 . . 
+    . . b 1 1 1 1 1 1 1 8 8 8 8 . . 
+    . . b 1 1 1 5 5 5 5 8 8 8 8 . . 
+    . . b 1 1 5 5 5 5 5 5 8 8 8 . . 
+    . . b 1 8 5 5 5 5 5 5 8 8 8 . . 
+    . . c 8 8 5 5 5 5 5 5 8 1 1 . . 
+    . . c 8 8 5 5 5 5 5 5 1 1 1 . . 
+    . . c 8 8 8 5 5 5 5 1 1 1 1 . . 
+    . . c 8 8 8 1 1 1 1 1 1 1 1 . . 
+    . . c 2 2 2 1 1 1 1 6 6 6 1 . . 
+    . . b 1 2 1 1 1 1 1 1 1 1 1 . . 
+    . . b 1 1 1 1 1 1 1 1 1 1 1 . . 
+    . . . b b b b b b b b b b . . . 
+    `,
+img`
+    . . . . . . . 6 . . . . . . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    . . 6 6 6 6 7 6 7 6 6 6 6 . . . 
+    . 6 6 7 6 6 7 6 7 6 6 7 7 6 . . 
+    . 6 6 7 6 7 6 6 7 6 6 7 6 6 . . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 7 6 7 6 7 6 . 
+    6 7 6 7 6 7 6 6 7 6 6 7 6 7 6 . 
+    6 6 6 7 6 7 6 6 7 6 6 7 6 6 6 . 
+    . 6 6 7 6 7 6 6 7 6 6 7 6 6 . . 
+    . 6 6 7 6 7 7 6 7 6 6 7 6 6 . . 
+    . . 6 6 6 6 7 6 7 6 6 6 6 . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    `
+]
+let groceryNames = [
+"Milk",
+"Grape Soda",
+"Oatmeal",
+"Turkey",
+"Fancy glass",
+"Chicken soup",
+"Sardines",
+"Flour",
+"Watermelon"
+]
+let groceryWeights = [
+8,
+2,
+1,
+12,
+0.5,
+0.5,
+0.5,
+5,
+10
+]
+let groceryCosts = [
+2,
+3,
+4,
+20,
+10,
+2,
+1,
+5,
+3
+]
+scene.setBackgroundColor(9)
+tiles.setTilemap(tilemap`level`)
+let player = sprites.create(img`
+    fffffff......................
+    f.fffcd......................
+    ..ffddc......................
+    ..fdddf......................
+    ..fdddd......................
+    ...55........................
+    ..55dddbbbbbbbbbbbb..........
+    ..555....b..b..b..b..........
+    .5555....bbbbbbbbbb..........
+    .5555....b..b..b..b..........
+    .5555....bbbbbbbbbb..........
+    .6666.....b.b..b.bb..........
+    .6.66......bbbbbbb...........
+    .d.d.......d.................
+    .d..d......ddddddd...........
+    .d..dd......c....c...........
+    `, SpriteKind.Player)
+controller.moveSprite(player)
+scene.cameraFollowSprite(player)
+// column = 0, row = 4: how do we place the sprite on column 0 row 4
+tiles.placeOnTile(player, tiles.getTileLocation(1, 3))
